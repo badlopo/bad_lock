8a8f7fda8e3dd3b9a63d7b93e1c46249aebaad60
lopo <lopo@zju.edu.cn>
2024-06-16 23:46:50 +0800

    feat: macro -- timestamp

--- src/macros.rs
+++ src/macros.rs
@@ -4,3 +4,13 @@ macro_rules! get_md5 {
         md5::compute($source).0
     };
 }
+
+#[macro_export]
+macro_rules! timestamp {
+    () => {
+        std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_millis()
+    };
+}
