b1ea4c1f7078e574aa4826930d8a2e4e884806ed
lopo <lopo@zju.edu.cn>
2024-06-17 22:35:28 +0800

    feat: BadLockIO::unlock fix: trait bound

--- src/io.rs
+++ src/io.rs
@@ -1,4 +1,3 @@
-use std::ffi::OsStr;
 use std::fs;
 use std::path::Path;
 use crate::r#impl::BadLockImpl;
@@ -8,12 +7,14 @@ pub struct BadLockIO;
 impl BadLockIO {
     /// - `input` path to the file to be locked
     /// - `password` password to lock the file
-    /// - `output` path to the output file
-    pub fn lock(
-        input: impl AsRef<Path>,
-        password: impl AsRef<[u8]>,
-        output: Option<impl AsRef<Path>>,
-    ) -> Result<(), String> {
+    /// - `output` path to the output file, if not provided,
+    /// the output file will be named as `input` with extension `.badlock`
+    pub fn lock<I, P, O>(input: I, password: P, output: Option<O>) -> Result<(), String>
+    where
+        I: AsRef<Path>,
+        P: AsRef<[u8]>,
+        O: AsRef<Path>,
+    {
         let input: &Path = input.as_ref();
 
         match fs::read(input) {
@@ -22,15 +23,49 @@ impl BadLockIO {
                 let bytes = BadLockImpl::lock(&bytes, password, ext);
 
                 let output = match output {
+                    Some(o) => o.as_ref().to_path_buf(),
                     None => {
                         let mut o = input.to_path_buf();
                         o.set_extension("badlock");
                         o
                     }
-                    Some(o) => o.as_ref().to_path_buf(),
                 };
 
                 match fs::write(output, bytes) {
+                    Ok(_) => Ok(()),
+                    Err(err) => Err(format!("Write error: {}", err)),
+                }
+            }
+            Err(err) => Err(format!("Read error: {}", err)),
+        }
+    }
+
+    /// - `input` path to the file to be unlocked
+    /// - `password` password to unlock the file
+    /// - `output` path to the output file, if not provided,
+    /// the output file will be named as `input` with extension in the locked file (if any)
+    pub fn unlock<I, P, O>(input: I, password: P, output: Option<O>) -> Result<(), String>
+    where
+        I: AsRef<Path>,
+        P: AsRef<[u8]>,
+        O: AsRef<Path>,
+    {
+        let input: &Path = input.as_ref();
+
+        match fs::read(input) {
+            Ok(bytes) => {
+                let result = BadLockImpl::unlock(&bytes, password)?;
+
+                let output = match output {
+                    Some(o) => o.as_ref().to_path_buf(),
+                    None => {
+                        let mut o = input.to_path_buf();
+                        o.set_extension(&result.extension);
+                        o
+                    }
+                };
+
+                match fs::write(output, result.content) {
                     Ok(_) => Ok(()),
                     Err(err) => Err(format!("Write error: {}", err)),
                 }
@@ -42,9 +77,17 @@ impl BadLockIO {
 
 #[cfg(test)]
 mod unit_test {
+    use std::path::Path;
+
     #[test]
     fn lock() {
         let r = super::BadLockIO::lock("test.txt", "password", Some("test.badlock"));
+        println!("{:?}", r)
+    }
+
+    #[test]
+    fn unlock() {
+        let r = super::BadLockIO::unlock("test.badlock", "password", None::<&Path>);
         println!("{:?}", r)
     }
 }
