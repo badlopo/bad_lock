8adcafa474a415a5f1e1c247efec965469cc1201
lopo <lopo@zju.edu.cn>
2024-06-17 21:35:57 +0800

    feat: BadLockImpl::unlock

--- src/impl.rs
+++ src/impl.rs
@@ -1,12 +1,7 @@
-//! io: provides I/O operations for BadLock
-
-use std::fs;
 use std::io::Write;
-use std::path::Path;
 use crate::core::BadLockCore;
-use crate::timestamp;
 
-const HEADER: &[u8] = b"BadLock(v0.0.1)\n";
+const AGENT: &[u8] = b"BadLock(v0.0.1)\n";
 
 /// Builds the header for the BadLock file.
 ///
@@ -22,10 +17,38 @@ fn build_header(extension: impl AsRef<[u8]>) -> Vec<u8> {
     let mut header = vec![32u8; 32];
     let extension: &[u8] = extension.as_ref();
 
-    header[..HEADER.len()].copy_from_slice(HEADER);
-    header[HEADER.len()..HEADER.len() + extension.len()].copy_from_slice(extension);
+    header[..AGENT.len()].copy_from_slice(AGENT);
+    header[AGENT.len()..AGENT.len() + extension.len()].copy_from_slice(extension);
 
     header
+}
+
+/// Parses the header of the BadLock file.
+///
+/// The header is fixed length of 32 bytes with the following format:
+///
+/// ```
+/// BadLock(v0.0.1)\n -- (16 bytes)
+/// <extension> -- (padding with spaces to 16 bytes)
+/// ```
+fn parse_header(header: &[u8]) -> Option<UnlockResult> {
+    let (executor, extension) = header.split_at(16);
+    if executor.starts_with(b"BadLock") {
+        Some(UnlockResult {
+            locker: String::from_utf8_lossy(executor).trim().to_string(),
+            extension: String::from_utf8_lossy(extension).trim().to_string(),
+            content: vec![],
+        })
+    } else {
+        None
+    }
+}
+
+#[derive(Debug)]
+pub struct UnlockResult {
+    pub locker: String,
+    pub extension: String,
+    pub content: Vec<u8>,
 }
 
 pub struct BadLockImpl;
@@ -40,18 +63,25 @@ impl BadLockImpl {
         header
     }
 
-    fn unlock(bytes: &[u8], password: impl AsRef<[u8]>) -> Result<(), String> {
-        todo!()
-        // match fs::read(path) {
-        //     Ok(bytes) => {
-        //         if bytes.starts_with(HEADER) {
-        //             return Err("Error: The file is already locked.".to_string());
-        //         }
-        //
-        //         Ok(())
-        //     }
-        //     Err(err) => Err(format!("{}", err))
-        // }
+    /// - `bytes` raw data to be unlocked
+    /// - `password` password to unlock the data
+    fn unlock(bytes: &[u8], password: impl AsRef<[u8]>) -> Result<UnlockResult, String> {
+        if bytes.len() < 32 {
+            return Err("Invalid BadLock file".to_string());
+        }
+
+        let (header, encrypted) = bytes.split_at(32);
+
+        match parse_header(header) {
+            Some(mut result) => match BadLockCore::decrypt(encrypted, password) {
+                Some(decrypted) => {
+                    result.content = decrypted;
+                    Ok(result)
+                }
+                None => Err("Invalid password".to_string()),
+            }
+            None => Err("Invalid BadLock file".to_string()),
+        }
     }
 }
 
@@ -64,10 +94,9 @@ mod unit_test {
     }
 
     #[test]
-    fn t() {
-        let mut v = vec![3, 4, 5];
-        v.splice(0..0, vec![1, 2, 3]);
-        v.extend_from_slice(&[6, 7, 8]);
-        println!("{:?}", v)
+    fn unlock() {
+        let bytes = [66, 97, 100, 76, 111, 99, 107, 40, 118, 48, 46, 48, 46, 49, 41, 10, 116, 120, 116, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 223, 56, 252, 85, 169, 5, 1, 161, 64, 150, 20, 114, 64, 51, 1, 179, 216, 157, 245, 248, 114, 58, 41, 110, 125, 77, 219, 126, 201, 25, 113, 96];
+        let decrypted = super::BadLockImpl::unlock(&bytes, "password");
+        println!("{:?}", decrypted);
     }
 }
